9226ca75da4cc31f386b797ba71ee4634a200bbd
OueslatiHassen <hassen.oueslati@esprit.tn>
2022-02-19 14:13:43 +0100

    Add Menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-
-      </Navbar>
+    <div >
+        <Navbar dark color="primary">
+            <div className="container">
+                <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+        </Navbar>
+        <Menu/>
 
     </div>
   );
